7c453af807f5aa7f669532c0145526ed2ade5e1e
John Webster <JohnLobster@comcast.net>
2020-06-07 01:41:01 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const path = require("path");
+require("dotenv").config(); // add variables in .env file to process.env
+const PORT = process.env.PORT || 3001;
+
+// check NODE_ENV
+console.log(`server: NODE_ENV ${process.env.NODE_ENV}`);
+// if NODE_ENV is undefined, that gets printed out before things start
+// crashing
+
+
+// set up express
+const app = express();
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static files (css)
+app.use(express.static("."));
+
+// serve author html (default)
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname,"authorIndex.html"));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server listening on port ${PORT}`);
+});
