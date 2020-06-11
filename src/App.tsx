ce91f030c897bc057164c9a400c2ce99e75c69b8
Me <johnlobster@comcast.net>
2020-06-11 15:06:42 -0700

    merging into master

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 
+function inIframe():boolean { try { return window.self !== window.top; } catch (e) { return true; } }
+
 function App() {
+
+  const [iFrame , updateIFrame] = React.useState( inIframe() ); // boolean
+
+
   return (
     <div>
-      <h1>Inside the iframe</h1>
-      <h2>Even more stuff inside the iframe</h2>
+      <h1>The web app</h1>
+      <h2>More stuff to make things happen</h2>
+      { iFrame ? (
+        <h1>Inside an Iframe</h1>
+      ) : (
+        <h1>Standalone</h1>
+
+      )}
       <h3>Want to get the iframe to scroll</h3>
       <h1>I want to scroll</h1>
       <h1>I want to scroll</h1>
