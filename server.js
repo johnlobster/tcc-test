67300818070a7d9854599c46e4808f52fef441e2
Me <johnlobster@comcast.net>
2020-06-12 12:41:49 -0700

    Test on netlify

--- server.js
+++ server.js
@@ -31,88 +31,87 @@ console.log("Original test data " + testData);
 // --depth 1  -> shallow clone, only get most recent 
 // <repo URL> <my directory> (e.g. github-source)
 
-// clone repo
-if (shell.exec(`git clone -b heroku-git-trial https://github.com/johnlobster/tcc-test.git github-repo`).code !== 0) {
-  shell.echo('Error: Git clone failed');
-  shell.exit(1);
-} else {
-  console.log("Successful clone to github-repo");
-}
+// // clone repo
+// if (shell.exec(`git clone -b heroku-git-trial https://github.com/johnlobster/tcc-test.git github-repo`).code !== 0) {
+//   shell.echo('Error: Git clone failed');
+//   shell.exit(1);
+// } else {
+//   console.log("Successful clone to github-repo");
+// }
 
-// let lsArray = shell.ls('github-repo');
-// console.log(`${lsArray.length} files in directory`);
-// lsArray.forEach( (value, index) => {console.log(value);})
+// // let lsArray = shell.ls('github-repo');
+// // console.log(`${lsArray.length} files in directory`);
+// // lsArray.forEach( (value, index) => {console.log(value);})
 
-const cdString = shell.cd("github-repo");
-console.log("Change directory to github-repo ShellString: " + cdString);
+// const cdString = shell.cd("github-repo");
+// console.log("Change directory to github-repo ShellString: " + cdString);
 
-// let lsArray2 = shell.ls();
-// console.log(`${lsArray2.length} files in directory`);
-// lsArray2.forEach((value, index) => { console.log(value); })
+// // let lsArray2 = shell.ls();
+// // console.log(`${lsArray2.length} files in directory`);
+// // lsArray2.forEach((value, index) => { console.log(value); })
 
-// check that we are on the right branch
-if (shell.exec('git branch').code !== 0) {
-  shell.echo('Error: Git branch failed');
-  shell.exit(1);
-}
+// // check that we are on the right branch
+// if (shell.exec('git branch').code !== 0) {
+//   shell.echo('Error: Git branch failed');
+//   shell.exit(1);
+// }
 
-// create file before adding to git repo
-let fileName = "t3_" + uniqid();
-fs.writeFileSync(fileName, JSON.stringify(testData));
+// // create file before adding to git repo
+// let fileName = "t3_" + uniqid();
+// fs.writeFileSync(fileName, JSON.stringify(testData));
 
-// add explicitly because new file
-if (shell.exec('git add ' + fileName).code !== 0) {
-  shell.echo('Error: Git add failed');
-  shell.exit(1);
-}
+// // add explicitly because new file
+// if (shell.exec('git add ' + fileName).code !== 0) {
+//   shell.echo('Error: Git add failed');
+//   shell.exit(1);
+// }
 
-// set up user name and email
-shell.exec("git config user.name 'johnlobster'");
-shell.exec("git config user.email 'JohnLobster@comcast.net'");
+// // set up user name and email
+// shell.exec("git config user.name 'johnlobster'");
+// shell.exec("git config user.email 'JohnLobster@comcast.net'");
 
-const dateStamp=new Date();
-console.log(dateStamp);
-const dsString = `${dateStamp.getHours()}${dateStamp.getMinutes()}${dateStamp.getSeconds()}`;
-if (shell.exec(`git commit -m "Auto-commit ${dsString}"`).code !== 0) {
-  shell.echo('Error: Git commit failed');
-  shell.exit(1);
-} else {
-  console.log('Git commit succeeded');
-  console.log();
-}
+// const dateStamp=new Date();
+// console.log(dateStamp);
+// const dsString = `${dateStamp.getHours()}${dateStamp.getMinutes()}${dateStamp.getSeconds()}`;
+// if (shell.exec(`git commit -m "Auto-commit ${dsString}"`).code !== 0) {
+//   shell.echo('Error: Git commit failed');
+//   shell.exit(1);
+// } else {
+//   console.log('Git commit succeeded');
+//   console.log();
+// }
 
-// console.log();
-// console.log("config file");
-// const cFile = shell.cat(".git/config");
-// console.log(cFile);
-// console.log();
+// // console.log();
+// // console.log("config file");
+// // const cFile = shell.cat(".git/config");
+// // console.log(cFile);
+// // console.log();
 
-if (shell.exec(`git push https://${ghToken}@github.com/johnlobster/tcc-test.git`).code !== 0) {
-  shell.echo('Error: Git push failed');
-  shell.exit(1);
-} else {
-  console.log('Git push succeeded');
-}
-
-// git testing
+// if (shell.exec(`git push https://${ghToken}@github.com/johnlobster/tcc-test.git`).code !== 0) {
+//   shell.echo('Error: Git push failed');
+//   shell.exit(1);
+// } else {
+//   console.log('Git push succeeded');
+// }
 
 // set up express
-// const app = express();
+const app = express();
 // Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// static files (css)
-// app.use(express.static("."));
+// static files (css or js etc.)
+app.use(express.static("."));
+
 
 // serve author html (default)
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname,"authorIndex.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname,"authorIndex.html"));
+});
 
 
-// app.listen(PORT, () => {
-//   console.log(`🌎 ==> API server listening on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server listening on port ${PORT}`);
+});
 
 console.log("Server ends here");
--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,38 @@ function App() {
 
   const [iFrame , updateIFrame] = React.useState( inIframe() ); // boolean
 
+  const receiveMessage = (event:any) => {
+    event.preventDefault(); // Probably doesn't bubble up
+    console.log(`IFRAME postMessage received from ${event.origin}`);
+    console.log(event.data); // might be an object so would be nice to pretty print
+    
+    // is console logging the source causing the issue ? Try sending a reply
+    // console.log("IFRAME Reply to");
+    // console.log(event.source);
+
+    // reply
+    event.source.postMessage(`A polite reply to ${event.data}`, "*");
+
+  }
+
+  // const sendMessage = () => {
+
+  // }
+
+  // on mount, listen to postMessage event
+  React.useEffect(() => {
+    console.log("IFRAME Mount App");
+    if ( iFrame) {
+      console.log("IFRAME Inside iframe, listen for postMessage event");
+      window.addEventListener("message", receiveMessage);
+      return(() => {
+        console.log("IFRAME Unmount App, remove message listener");
+        window.removeEventListener("message", receiveMessage);
+      });
+    } else {
+      return;
+    }
+    })
 
   return (
     <div>
